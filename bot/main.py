515e40c1fc15c6c1ca3bf9dd5fb859dbbac3242f
Jalil Artykov <artykovjalil@gmail.com>
2025-02-11 02:40:06 +0600

    add notification for start and stop bot

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,9 @@
+import logging
 from aiogram import Bot, Dispatcher
-from config.config import BOT_TOKEN
-
+from config.config import BOT_TOKEN, MY_CHAT_ID
 from bot.routers.start.router import router as start_router
+
+logger = logging.getLogger(__name__)
 
 bot = Bot(token=BOT_TOKEN)
 
@@ -10,5 +12,28 @@ dp = Dispatcher()
 dp.include_router(start_router)
 
 
+
+
+async def send_startup_notification():
+    """Отправляет сообщение в чат при старте бота"""
+    try:
+        await bot.send_message(MY_CHAT_ID, "✅ Бот запущен и работает!")
+    except Exception as e:
+        logger.error(f"Ошибка при отправке уведомления о старте: {e}")
+
+async def send_shutdown_notification():
+    """Отправляет сообщение в чат перед остановкой бота"""
+    try:
+        await bot.send_message(MY_CHAT_ID, "⚠️ Бот остановлен!")
+    except Exception as e:
+        logger.error(f"Ошибка при отправке уведомления о выключении: {e}")
+
+
 async def start_bot():
-    await dp.start_polling(bot)
+    """Запуск бота с уведомлением"""
+    await send_startup_notification()  # Отправляем уведомление о запуске
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await send_shutdown_notification()  # Уведомление при остановке
+        await bot.session.close()  # Закрываем сессию бота
